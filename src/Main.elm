38c8b30503d73fa122bdf5812681d5bced953eb6
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-06-14 00:12:26 -0300

    Begin hashing implementation

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,22 @@
 
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-main = Browser.sandbox { init = init, view = view, update = update }
+main = Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
+-- Ports
+
+port askHash : (String, String) -> Cmd msg
+port onHash : (String -> msg) -> Sub msg
+
+-- Structure
 
 type HashAlgorithm = SHA256 | SHA1
-
 algorithmName : HashAlgorithm -> String
 algorithmName algo = case algo of
     SHA256 -> "SHA256"
@@ -17,28 +24,45 @@ algorithmName algo = case algo of
 
 type alias Model = {
         algorithm: Maybe HashAlgorithm,
-        inputText: String
+        inputText: String,
+        computedHash: Maybe String
     }
 
-type Msg = SwitchAlgorithm String
+type Msg =
+    SwitchAlgorithm String
     | InputTyped String
+    | HashComputed String
 
 
-init : Model
-init = {
+init : () -> ( Model, Cmd Msg )
+init _ = {
     algorithm = Nothing,
-    inputText = ""
-    }
+    inputText = "",
+    computedHash = Nothing
+    } |> pure
 
-update : Msg -> Model -> Model
+
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update message model =
     case message of
         SwitchAlgorithm (algo) -> case algo of
-            "SHA256" -> { model | algorithm = Just SHA256 }
-            "SHA1" -> { model | algorithm = Just SHA1 }
-            _ -> { model | algorithm = Nothing }
+            "SHA256" -> { model | algorithm = Just SHA256 } |> pure
+            "SHA1" -> { model | algorithm = Just SHA1 } |> pure
+            _ -> { model | algorithm = Nothing } |> pure
 
-        InputTyped (str) -> { model | inputText = str }
+        InputTyped (str) -> ({ model | inputText = str },
+            case model.algorithm of
+                Nothing -> Cmd.none
+                Just algo -> askHash (algorithmName algo, str))
+
+        HashComputed (str) -> { model | computedHash = Just str } |> pure
+
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = onHash HashComputed
 
 
 view : Model -> Html Msg
@@ -60,18 +84,24 @@ view model = div []
                     option [value "SHA1"] [ text "SHA1" ]
                 ],
 
-                input [placeholder "Type some input", onInput InputTyped ] [  ],
+                input [placeholder "Type some input", onInput InputTyped] [  ],
 
                 case model.algorithm of
                     Nothing -> div [] []
-                    (Just algo) -> div []
+                    Just algo -> div []
                         [ algo |> algorithmName |> text,
                           text " Hash: ",
-                          code [] [
-                              text ("hash(" ++ algorithmName algo ++ "," ++ model.inputText ++ ")")
-                          ]
+                          case model.computedHash of
+                              Nothing -> text "...computing"
+                              Just str -> code [] [text str]
                         ]
             ]
-
         ]
     ]
+
+-- decoders
+
+-- utility
+
+pure : a -> (a, Cmd msg)
+pure x = (x, Cmd.none)
